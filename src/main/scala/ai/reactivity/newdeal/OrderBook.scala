e74708bd5164d63640bc65891a85ea003d093368
Alexander Temerev <sorhed@gmail.com>
2019-05-24 23:21:06 +0200

    WIP

--- src/main/scala/ai/reactivity/newdeal/OrderBook.scala
+++ src/main/scala/ai/reactivity/newdeal/OrderBook.scala
@@ -1,5 +1,7 @@
 package ai.reactivity.newdeal
 
+import ai.reactivity.newdeal.math.SafeDouble
+
 case class OrderBook private(symbol: Symbol, bids: List[Entry], offers: List[Entry]) {
   def +(entry: Entry): OrderBook = {
     val line = if (entry.isBid) bids else offers
@@ -7,6 +9,14 @@ case class OrderBook private(symbol: Symbol, bids: List[Entry], offers: List[Ent
     val insert = if (idx == -1) line.length else idx
     val newLine = line.patch(insert, List(entry), 0)
     if (entry.isBid) copy(bids = newLine) else copy(offers = newLine)
+  }
+
+  def removeByPrice(price: SafeDouble): OrderBook = {
+    val line = if (price < 0) bids else offers
+    val priceStart = line.indexWhere(_.price == price)
+    if (priceStart == -1) this else {
+      ???
+    }
   }
 }
 
